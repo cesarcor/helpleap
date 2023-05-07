af36cac7306d906b453d8a1ed9050e7cc095639f
cesarcor <cesarcorrechel@gmail.com>
2023-05-06 21:49:04 -0600

    IC: add TicketInterventionStatus(ticket item) comp

---
+++ src/components/tickets/ticket-intervention-status/ticket-intervention-status.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ticket-intervention-status.module.scss';
+
+const TicketInterventionStatus = () => {
+	return (
+		<div className={styles.ticket_intervention_status}>
+			<span>Normal</span>
+		</div>
+	);
+};
+
+export default TicketInterventionStatus;
