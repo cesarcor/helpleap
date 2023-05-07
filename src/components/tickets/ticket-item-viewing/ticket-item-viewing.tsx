3dc9d9cffcd3585d87ac70acadd40db5ebdc9b4b
cesarcor <cesarcorrechel@gmail.com>
2023-05-06 21:49:34 -0600

    IC: add TicketItemViewing(ticket item) comp

---
+++ src/components/tickets/ticket-item-viewing/ticket-item-viewing.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './ticket-item-viewing.module.scss';
+import RoundedImage from '@/components/common-ui/rounded-image/rounded-image';
+
+const TicketItemViewing = () => {
+	return (
+		<div className={styles.ticket_viewing}>
+			<span>Viewing:</span>
+			<ul className={styles.viewing_list}>
+				<li>
+					<RoundedImage width='20px' height='20px' />
+				</li>
+				<li>
+					<RoundedImage width='20px' height='20px' />
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default TicketItemViewing;
