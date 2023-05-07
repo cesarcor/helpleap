0e7eac0d19890e70a197253a2d17d6fb3c0c4524
cesarcor <cesarcorrechel@gmail.com>
2023-05-06 21:47:52 -0600

    IC: add assigned user comp

---
+++ src/components/tickets/assigned-user/assigned-user.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './assigned-user.module.scss';
+
+const AssignedUser = () => {
+	return (
+		<div className={styles.assigned_user}>
+			<span className={styles.assigned_user_name}>A.I.</span>
+		</div>
+	);
+};
+
+export default AssignedUser;
