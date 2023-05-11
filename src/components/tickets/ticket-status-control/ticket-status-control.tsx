9f208847eb400e790e777fdff0ede3aed2402082
cesarcor <cesarcorrechel@gmail.com>
2023-05-10 22:42:58 -0600

    add TicketStatusControl comp.

---
+++ src/components/tickets/ticket-status-control/ticket-status-control.tsx
@@ -0,0 +1,21 @@
+import styles from './ticket-status-control.module.scss';
+import Button from '@/components/common-ui/button/button';
+import Badge from '@/components/common-ui/badge/badge';
+import Select from '@/components/common-ui/select/select';
+
+const TicketStatusControl = () => {
+	return (
+		<div className={styles.ticket_status_control}>
+			<div className={styles.left}>
+				<Badge text='#ME345' type='info' />
+				<Button text='Follow' type='default' />
+			</div>
+			<div className={styles.right}>
+				<Select />
+				<Select />
+			</div>
+		</div>
+	);
+};
+
+export default TicketStatusControl;
