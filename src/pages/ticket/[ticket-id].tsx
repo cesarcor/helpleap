76fa40adcd784e6fc5cf31a7db784cfc82ec3e6c
cesarcor <cesarcorrechel@gmail.com>
2023-05-10 22:43:22 -0600

    add main pages

---
+++ src/pages/ticket/[ticket-id].tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Layout from '@/components/layout/layout';
+import Box from '@/components/common-ui/box/box';
+import SingleTicket from '@/components/tickets/single-ticket/single-ticket';
+import TicketStatusControl from '@/components/tickets/ticket-status-control/ticket-status-control';
+
+export default function Ticket() {
+	return (
+		<>
+			<Head>
+				<title>AI Helpdesk</title>
+				<meta name='description' content='Ticket view' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Layout>
+				<TicketStatusControl />
+				<Box min_height='550px'>
+					<SingleTicket />
+				</Box>
+			</Layout>
+		</>
+	);
+}
