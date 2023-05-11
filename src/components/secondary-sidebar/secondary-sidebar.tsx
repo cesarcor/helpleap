b56df82560a65ec08148d7fd3d52d748cc9905d8
cesarcor <cesarcorrechel@gmail.com>
2023-05-11 10:51:48 -0600

    implement conditional display of 'widgets' comps based on route

--- src/components/secondary-sidebar/secondary-sidebar.tsx
+++ src/components/secondary-sidebar/secondary-sidebar.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
 import styles from './secondary-sidebar.module.scss';
 import TrendsWidget from '../trends-widget/trends-widget';
+import TicketCategorizationWidget from '../tickets/ticket-categorization-widget/ticket-categorization-widget';
+import { useRouter } from 'next/router';
+import TicketCustomerInfoWidget from '../tickets/ticket-customer-info-widget/ticket-customer-info-widget';
 
 const SecondarySidebar = () => {
+	const router = useRouter();
+	const currentRoute = router.pathname;
+
+	console.log(currentRoute);
+
 	return (
 		<div className={`${styles.secondary_sidebar}`}>
-			<TrendsWidget />
+			<div className={styles.secondary_sidebar__container}>
+				{currentRoute === '/' ? <TrendsWidget /> : ''}
+				{currentRoute === '/ticket/[ticket-id]' ? (
+					<TicketCategorizationWidget />
+				) : (
+					''
+				)}
+				{currentRoute === '/ticket/[ticket-id]' ? (
+					<TicketCustomerInfoWidget />
+				) : (
+					''
+				)}
+			</div>
 		</div>
 	);
 };
