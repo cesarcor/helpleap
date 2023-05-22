963ace240bb3302d48dfbabad3242ff664e14a58
cesarcor <cesarcorrechel@gmail.com>
2023-05-21 19:57:16 -0600

    incorporate logo

--- src/components/main-sidebar/main-sidebar.tsx
+++ src/components/main-sidebar/main-sidebar.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import NavigationMenu from '../navigation-menu/navigation-menu';
 import styles from './main-sidebar.module.scss';
+import Image from 'next/image';
 
 const MainSidebar = () => {
 	return (
 		<div className={`${styles.main_sidebar}`}>
+			<div className={`${styles.logo}`}>
+				<Image src='/images/logo.png' alt='logo' width={50} height={50} />
+			</div>
 			<NavigationMenu />
 		</div>
 	);
