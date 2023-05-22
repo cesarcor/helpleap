9adeb153560fb73a5003921f392063b46fc0384f
cesarcor <cesarcorrechel@gmail.com>
2023-05-21 19:57:52 -0600

    incorporate svgs to individual menu items

--- src/components/navigation-menu/navigation-menu.tsx
+++ src/components/navigation-menu/navigation-menu.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import styles from './navigation-menu.module.scss';
 import Link from 'next/link';
+import TicketsSVG from '../svgs/navigation/tickets-svg';
+import CustomersSVG from '../svgs/navigation/customers-svg';
+import TrendsSVG from '../svgs/navigation/trends-svg';
+import SettingsSVG from '../svgs/navigation/settings-svg';
 
 const NavigationMenu = () => {
 	return (
 		<nav className={`${styles.nav_menu}`}>
 			<ul className={`${styles.nav_menu__list}`}>
 				<li className={`${styles.nav_menu__list_item}`}>
-					<Link href='/'>Tickets</Link>
+					<Link href='/'>
+						<TicketsSVG />
+						Tickets
+					</Link>
 				</li>
 				<li className={`${styles.nav_menu__list_item}`}>
-					<Link href='/customers'>Customers</Link>
+					<Link href='/customers'>
+						<CustomersSVG />
+						Customers
+					</Link>
 				</li>
 				<li className={`${styles.nav_menu__list_item}`}>
-					<Link href='/trends'>Trends</Link>
+					<Link href='/trends'>
+						<TrendsSVG />
+						Trends
+					</Link>
 				</li>
 				<li className={`${styles.nav_menu__list_item}`}>
-					<Link href='/settings'>Settings</Link>
+					<Link href='/settings'>
+						<SettingsSVG />
+						Settings
+					</Link>
 				</li>
 			</ul>
 		</nav>
