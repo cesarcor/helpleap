04d96416f05f0fbb336ffc43ca6d3df5e62a28fe
cesarcor <cesarcorrechel@gmail.com>
2023-05-24 09:39:44 -0600

    add basic comp. ui

--- src/components/trends-widget/trends-widget.tsx
+++ src/components/trends-widget/trends-widget.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import styles from './trends-widget.module.scss';
+import Link from 'next/link';
+import SmileSVG from '../svgs/common/smile-svg';
 
 const TrendsWidget = () => {
 	return (
 		<div className={`${styles.trends_widget}`}>
 			<h2>Trends</h2>
+			<div className={styles.trends_widget__list}>
+				<ul>
+					<li>
+						<Link href='#'>
+							<SmileSVG />
+							Omne animal, simul atque natum sit, a natura incorrupte atque
+							insitam in gravissimo bello.
+						</Link>
+					</li>
+					<li>
+						<Link href='#'>
+							<SmileSVG />
+							Omne animal, simul atque natum sit, a natura incorrupte atque
+							insitam in gravissimo bello.
+						</Link>
+					</li>
+					<li>
+						<Link href='#'>
+							<SmileSVG />
+							Omne animal, simul atque natum sit, a natura incorrupte atque
+							insitam in gravissimo bello.
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
