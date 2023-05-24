8bdfdc82097f7102ad2b64433f32992cabf75a59
cesarcor <cesarcorrechel@gmail.com>
2023-05-24 09:40:39 -0600

    add Heading comp.

---
+++ src/components/common-ui/heading/heading.tsx
@@ -0,0 +1,11 @@
+import styles from './heading.module.scss';
+
+const Heading = () => {
+	return (
+		<div className={styles.heading}>
+			<h1>Tickets</h1>
+		</div>
+	);
+};
+
+export default Heading;
