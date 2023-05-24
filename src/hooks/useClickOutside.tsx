87ae2a18e97edb14ab19ceeb253c68fb676b1bad
cesarcor <cesarcorrechel@gmail.com>
2023-05-24 09:42:21 -0600

    add useClickOutside hook

---
+++ src/hooks/useClickOutside.tsx
@@ -0,0 +1,23 @@
+import { useRef, useEffect } from 'react';
+
+const useClickOutside = (callback: () => void) => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (ref.current && !ref.current.contains(event.target as Node)) {
+				callback();
+			}
+		};
+
+		window.addEventListener('click', handleClickOutside);
+
+		return () => {
+			window.removeEventListener('click', handleClickOutside);
+		};
+	}, []);
+
+	return { ref };
+};
+
+export default useClickOutside;
