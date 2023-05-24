27c21696707a2d993c21fa19c04c9ba17a55c85f
cesarcor <cesarcorrechel@gmail.com>
2023-05-24 09:38:06 -0600

    incorporate Notifications & UserInfo comps.

--- src/components/secondary-sidebar/secondary-sidebar.tsx
+++ src/components/secondary-sidebar/secondary-sidebar.tsx
@@ -3,6 +3,8 @@ import TrendsWidget from '../trends-widget/trends-widget';
 import TicketCategorizationWidget from '../tickets/ticket-categorization-widget/ticket-categorization-widget';
 import { useRouter } from 'next/router';
 import TicketCustomerInfoWidget from '../tickets/ticket-customer-info-widget/ticket-customer-info-widget';
+import Notifications from '../notifications/notifications';
+import UserInfo from '../user-info/user-info';
 
 const SecondarySidebar = () => {
 	const router = useRouter();
@@ -13,6 +15,11 @@ const SecondarySidebar = () => {
 	return (
 		<div className={`${styles.secondary_sidebar}`}>
 			<div className={styles.secondary_sidebar__container}>
+				<div className={styles.user_bar}>
+					<Notifications />
+					<UserInfo />
+				</div>
+
 				{currentRoute === '/' ? <TrendsWidget /> : ''}
 				{currentRoute === '/ticket/[ticket-id]' ? (
 					<TicketCategorizationWidget />
