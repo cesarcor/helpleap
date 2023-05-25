c09831b87eed22e974de9b16ae239be745553416
cesarcor <cesarcorrechel@gmail.com>
2023-05-25 13:10:41 -0600

    incorporate EditorResponse comp.

--- src/components/tickets/single-ticket/response/response.tsx
+++ src/components/tickets/single-ticket/response/response.tsx
@@ -1,10 +1,12 @@
 import styles from './response.module.scss';
 import AutoResponse from './auto-response/auto-response';
+import EditorResponse from './editor-response/editor-response';
 
 const Response = () => {
 	return (
 		<div className={styles.response_area}>
-			<AutoResponse />
+			{/* <AutoResponse /> */}
+			<EditorResponse />
 		</div>
 	);
 };
