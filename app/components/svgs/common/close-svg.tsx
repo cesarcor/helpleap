3aa605e7ce1e3913df2a69b8a01e2b6ce873c7d6
cesarcor <cesarcorrechel@gmail.com>
2023-07-01 21:58:35 -0600

    add close svg

---
+++ app/components/svgs/common/close-svg.tsx
@@ -0,0 +1,21 @@
+function CloseSVG() {
+	return (
+		<svg
+			width='7'
+			height='7'
+			viewBox='0 0 8.00381904 10.0186319'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M1.04 0v.004A.998.998 0 00.208 1.61l2.544 3.391L.208 8.397a1 1 0 001.6 1.198l2.19-2.923 2.19 2.924a.999.999 0 101.599-1.198L5.246 5.002l2.54-3.39A.998.998 0 106.19.414L3.998 3.338 1.807.414A.998.998 0 001.04 0z'
+				transform='translate(-640 -118) translate(640 118)'
+				fill='#B6BDCF'
+				fillRule='nonzero'
+				stroke='none'
+				strokeWidth={1}
+			/>
+		</svg>
+	);
+}
+
+export default CloseSVG;
